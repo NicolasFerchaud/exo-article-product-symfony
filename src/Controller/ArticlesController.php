d2400f629e9194dd5a826f32a43b7c10faffda03
Nicolas F <nicolasferchaud@hotmail.fr>
2021-03-19 14:36:45 +0100

    comment

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -12,22 +12,22 @@ class ArticlesController extends AbstractController
     /**
      * @Route("/articles", name="articles")
      */
-    public function list_articles(ArticleRepository $articleRepository)
+    public function list_articles(ArticleRepository $articleRepository)//ArticleRepository est une class instanciée dans la methode accueil
     {
-        $articles = $articleRepository->findAll();
-        return $this->render('articles.html.twig',[
-            'articles'=> $articles
+        $articles = $articleRepository->findAll();//recupère tous les éléments de la table (requète native)
+        return $this->render('articles.html.twig',[//fait un rendu de homeController .twig en .html pour être lu par le navigateur
+            'articles'=> $articles//je créé une variable qui à pour valkeur le contenu de $articles
             ]);
     }
 
     /**
      * @Route("article/{id}", name="article")
      */
-    public function article(ArticleRepository $articleRepository, $id)
+    public function article(ArticleRepository $articleRepository, $id)//ArticleRepository est une class instanciée dans la methode accueil + wildcard
     {
-        $article = $articleRepository->find($id);
-        return $this->render('article_detail.html.twig',[
-                'article' => $article
+        $article = $articleRepository->find($id);//recupère un éléments de la table grace à son id (requète native)
+        return $this->render('article_detail.html.twig',[//fait un rendu de homeController .twig en .html pour être lu par le navigateur
+                'article' => $article//je créé une variable qui à pour valkeur le contenu de $articles
             ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,15 +12,15 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function accueil(ArticleRepository $articleRepository)
+    public function accueil(ArticleRepository $articleRepository)//ArticleRepository est une class instanciée dans la methode accueil
     {
-        $articles = $articleRepository->findBy(
+        $articles = $articleRepository->findBy(//recupère tous les éléments de la table enfonction de certain paramètre (requète native)
             ['isPublished' => true],
             ['createdAt' => 'DESC'],
             2
         );
-        return $this->render('home.html.twig',[
-            'articles'=> $articles
+        return $this->render('home.html.twig',[//fait un rendu de homeController .twig en .html pour être lu par le navigateur
+            'articles'=> $articles//je créé une variable qui à pour valkeur le contenu de $articles
         ]);
     }
 }
