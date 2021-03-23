bd0364ea43914c3919af341cc610e936e6cb4dde
Nicolas F <nicolasferchaud@hotmail.fr>
2021-03-23 11:29:24 +0100

    exo final

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -4,6 +4,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,12 +14,12 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin/articles/insert",name="insert_article")
      */
-    public function insertArticle(EntityManagerInterface $entityManager)
+    public function insertArticle(EntityManagerInterface $entityManager)//entityManager gère les entités pour créé ou modifié un article
     {
         // je créé un nouvel objet que je stock dans une variable
         $article = new Article();
         //je met toute les valeurs à remplir dans mon INSERT
-        $article-> setTitle('admin insert');
+        $article-> setTitle('super article');
         $article->setContent('premier essai');
         $article->setCreatedAt(new \DateTime('NOW'));
         $article->setIsPublished(true);
@@ -30,6 +31,43 @@ class AdminController extends AbstractController
 
         return $this->render('admin_article_insert.html.twig',[
             'insert' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/admin/articles/update/{id}", name="update")
+     */
+    public function updateArticle(EntityManagerInterface $entityManager, ArticleRepository $articleRepository, $id)
+    {
+        $article = $articleRepository->find($id);//j'enregistre dans une variable l'id d'un article que je recupère en bdd
+
+        if (is_null($article)) {
+            throw $this->createNotFoundException('article non trouvée');//si l'id correspond a rien je retourne un message d'erreur
+        }
+
+        $article->setContent('première modif');//je fais la MAJ que je souhaite sur mon article
+
+        $entityManager->flush();//j'envoi la requête
+
+        return $this->render('admin_article_update.html.twig',[//je retourne le tout sur une vue html dédié
+            'update' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/admin/articles/delete/{id}", name="delete")
+     */
+    public function deleteArticle($id, EntityManagerInterface $entityManager, ArticleRepository $articleRepository)
+    {
+        $article = $articleRepository->find($id);//j'enregistre dans une variable l'id d'un article que je recupère en bdd
+        if (is_null($article)) {
+            throw $this->createNotFoundException('article non trouvée');//si l'id correspond a rien je retourne un message d'erreur
+        }
+        $entityManager->remove($article);//je surveille les fichier à effacer
+        $entityManager->flush();//j'envoi la requete
+
+        return $this->render('admin_article_delete.html.twig',[
+            'delete' => $article
         ]);
     }
 }
---
+++ src/Controller/exo_final/finalController.php
@@ -0,0 +1,81 @@
+<?php
+
+
+namespace App\Controller\exo_final;
+
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class finalController extends AbstractController
+{
+    /**
+     * @Route ("/admin/list_article", name="list_article")
+     */
+    public function list_article(ArticleRepository $articleRepository)
+    {
+        $article = $articleRepository->findAll();
+        return $this->render('exo_final/list_article.html.twig',[
+            'LIST' => $article
+        ]);
+    }
+
+    /**
+     * @Route ("/admin/insert", name="insert")
+     */
+    public function insert(EntityManagerInterface $entityManager)
+    {
+        $article = new Article();
+        $article->setTitle('title exo FINAL');
+        $article->setContent('content exo FINAL');
+        $article->setCreatedAt(new \DateTime('NOW'));
+        $article->setIsPublished(true);
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return $this->render('exo_final/INSERT.html.twig',[
+            'INSERT' => $article
+        ]);
+    }
+
+    /**
+     * @Route ("/admin/update/{id}", name="update")
+     */
+    public function update(EntityManagerInterface $entityManager, ArticleRepository $articleRepository, $id)
+    {
+        $article = $articleRepository->find($id);
+        if (is_null($article)){
+            throw $this->createNotFoundException('article non trouvé');
+        }
+
+        $article->setTitle('NEW title FINAL');
+
+        $entityManager->flush();
+
+        return $this->render('exo_final/UPDATE.html.twig',[
+            'UPDATE' => $article
+        ]);
+    }
+
+    /**
+     * @Route ("/admin/delete/{id}", name="delete")
+     */
+    public function delete(ArticleRepository $articleRepository, EntityManagerInterface $entityManager, $id)
+    {
+        $article = $articleRepository->find($id);
+        if (is_null($article)){
+            throw $this->createNotFoundException('article non trouvé');
+        }
+
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        return $this->render('exo_final/DELETE.html.twig',[
+            'DELETE' => $article
+        ]);
+    }
+}
